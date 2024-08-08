821a91821e4f24bbaa8f0b3952e5ca202572aa80
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-08-08 20:29:25 +0000

    sssssssssssssssss

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".resultado-1");
-const caixaResultado2 = document.querySelector(".resultado-2");
+
 const textoResultado = document.querySelector(".textoResultado");
 const botao1 = document.querySelector(".caixa-alternativas .botao_1");
-const botao2 = document.querySelector(".caixa-alternativas .botao_2");
+
 
 
 
@@ -20,11 +20,7 @@ botao1.addEventListener("click", function(){
     
 })
 
-botao2.addEventListener("click", function(){
-    console.dir(caixaResultado); /*Mostra o elemento html em formato de objeto*/
-    caixaResultado2.classList.add("mostrar-caixa-resultado");
-    
-})
+
 
 
 
